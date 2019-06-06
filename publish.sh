c9f5093f0e358545c4240bb0d2e004f8983c5950
James Britton <jhbritton96@icloud.com>
2019-06-06 01:18:33 +0100

    Fixed some name spelling mistakes. But still having isses findin the files to add to the tag.

--- publish.sh
+++ publish.sh
@@ -16,13 +16,12 @@ dotnet publish -c Release -r ubuntu.16.10-x64
 
 cd ../$ReleaseDir/netcoreapp2.1
 
-zip -r NameSorter_Windos$revision.zip win10-x64/*
+zip -r NameSorter_Windows_$revision.zip win10-x64/*
 zip -r NameSorter_Linux_$revision.zip ubuntu.16.10-x64/*
-zip -r NameSorter.zip NameSorter_Windos$revision.zip NameSorter_Linux_$revision.zip
+zip -r NameSorter_All.zip NameSorter_Windows_$revision.zip NameSorter_Linux_$revision.zip
 
 #cp -r NameSorter.zip ../../../../
-
-export RELEASE_PKG_FILE=$(ls *.zip)
-echo "Deploying $RELEASE_PKG_FILE to GitHub as a Tag"
-
 cd ../../../../
+
+export RELEASE_PKG_FILE=$(ls NameSorter/bin/Release/netcoreapp2.1/*.zip)
+echo "Deploying $RELEASE_PKG_FILE to GitHub as a Tag"
